eda8e2c8be00129d99bc80852afa4eadc1532d0f
purplelemons-dev <ironbreaker2005@gmail.com>
2024-02-02 18:57:06 -0600

    works

--- phase1.py
+++ phase1.py
@@ -14,28 +14,39 @@ def hashEmbed(embed: np.ndarray) -> int:
 SIZE = 2**25
 DIMS = 3072
 TEXT_SIZE = 26
+
 client = OpenAI()
 cl100k = cl100k_base()
-tokens = set(i[:TEXT_SIZE] for i in cl100k["mergeable_ranks"].keys())
+
+tokens: set[bytes] = set(i[:TEXT_SIZE] for i in cl100k["mergeable_ranks"].keys())
 tokens.update(b"<|endoftext|>")
 
 embed_to_token: np.ndarray = np.ndarray((SIZE,), dtype=f"|S{TEXT_SIZE}")
 embeddings = np.ndarray((len(tokens), DIMS), dtype=np.float64)
 
-for idx, token in enumerate(tokens):
-    embedding = np.array(
-        client.embeddings.create(
-            model="text-embedding-3-large", dimensions=DIMS, input=token
+intermediate:list[list[str]] = []
+current = []
+for i in tokens:
+    if len(current) >= 512:
+        intermediate.append(current)
+        current = []
+    current.append(str(i))
+
+amount_done = 0
+
+for idx, token_list in enumerate(intermediate):
+    print(f"{amount_done=}")
+    for token, embedding_data in zip(token_list, client.embeddings.create(
+            model="text-embedding-3-large", dimensions=DIMS, input=token_list
         )
-        .data[0]
-        .embedding,
-        dtype=np.float64,
-    )
-    norm = np.linalg.norm(embedding)
-    if norm > 0:
-        embedding /= norm
-    embed_to_token[hashEmbed(embedding)] = token
-    embeddings[idx] = embedding
+        .data):
+        embedding = np.array(embedding_data.embedding, dtype=np.float64)
+        norm = np.linalg.norm(embedding)
+        if norm > 0:
+            embedding /= norm
+        embed_to_token[hashEmbed(embedding)] = token
+        embeddings[idx] = embedding
+        amount_done += 1
 
 
 np.save("embed_to_token.npy", embed_to_token)
