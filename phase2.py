4b4ae0b75db56bf5e4488704aa2e8a8a828fb6e3
purplelemons-dev <ironbreaker2005@gmail.com>
2024-02-02 19:58:29 -0600

    kinda works

--- phase2.py
+++ phase2.py
@@ -6,9 +6,6 @@ from phase1 import normalize, hashEmbed
 embed_to_token = np.load("embed_to_token.npy")
 embeddings: np.ndarray = np.load("embeddings.npy")
 
-print(f"{embed_to_token=}")
-print(f"{embeddings=}")
-
 client = OpenAI()
 
 
@@ -18,19 +15,15 @@ def gen_next_best(target:np.ndarray):
         hash_val = hashEmbed(embed)
         options[embed_to_token[hash_val]] = 1 - np.dot(target, embed)
 
-    return min(options, key=options.get)
+    return str(min(options, key=options.get)).replace('b"b\'',"").replace('\'"','')
 
 if __name__ == "__main__":
-    user_in = input("Enter a string: ")
-    print(repr(user_in))
-    target = normalize(
-        np.array(
-            client.embeddings.create(
-                model="text-embedding-3-large", input=user_in
-            )
-            .data[0]
-            .embedding,
-            dtype=np.float64,
-        )
-    )
-    print(f"Next best token: {gen_next_best(target)}")
+    while True:
+        user_in = input("Enter a string: ")
+        out = user_in
+        while not out.endswith("<|endoftext|>"):
+            print(out)
+            out += gen_next_best(client.embeddings.create(
+                model="text-embedding-3-large", input=out
+            ).data[0].embedding)
+        print(out)
